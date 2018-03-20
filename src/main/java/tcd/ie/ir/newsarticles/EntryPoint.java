481f340c4bbb32c453fe7d4f4dc48ce10cc81e80
sahil <manchans@tcd.ie>
2018-03-20 18:55:08 +0000

    adding queryfire

--- src/main/java/tcd/ie/ir/newsarticles/EntryPoint.java
+++ src/main/java/tcd/ie/ir/newsarticles/EntryPoint.java
@@ -3,9 +3,23 @@ package tcd.ie.ir.newsarticles;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
 
 import tcd.ie.ir.newsarticles.entity.QueryObject;
 import tcd.ie.ir.newsarticles.impl.SearchIndexer;
@@ -56,6 +70,12 @@ public class EntryPoint
 		
 		// Print the query output
 		printOutput(queryObj);
+		try {
+			fireQuery(401,queryObj);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
@@ -63,5 +83,32 @@ public class EntryPoint
 		for(Map.Entry<Integer, QueryObject> entry: queryObj.entrySet()) {
 			System.out.println(entry.getValue().toString());
 		}
+	}
+	
+	public static void fireQuery(int index, Map<Integer, QueryObject> queryObj) throws Exception {
+		
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(FileUtils.getDocIndexPath())));
+	    IndexSearcher searcher = new IndexSearcher(reader);
+	    Analyzer analyzer = new PerFieldAnalyzerWrapper(new StandardAnalyzer());;
+	    String field = "contents";
+	    for(Map.Entry<Integer, QueryObject> entry: queryObj.entrySet()) {
+	    	int querynumber = entry.getKey();
+	    	String query = entry.getValue().getTitle();
+	    	QueryParser queryParser = new QueryParser(field,analyzer);
+	    	TopDocs results = searcher.search(queryParser.parse(query), 100);
+	        ScoreDoc[] hits = results.scoreDocs;
+	        File file = new File(FileUtils.getResultantPath()+"/results");
+	        PrintWriter pw = new PrintWriter(file);
+	        for(int i=0;i<hits.length;i++) {
+	        	int s = hits[i].doc;
+	        	Document d = searcher.doc(s);
+	        	int j = i+1;
+	        	pw.println(querynumber + " Q0 " +  d.get("ID") + " " + j + " " + hits[i].score + " STANDARD");
+	        	pw.flush();
+	        }
+	        int numTotalHits = Math.toIntExact(results.totalHits);
+	        System.out.println(numTotalHits + " total matching documents");
+	    }
+	    
 	}
 }
